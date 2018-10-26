68af7822cefc94439f01477c026fd1635f3ad363
Dan <40025131+webxdata@users.noreply.github.com>
2018-10-27 00:09:35 +0200

    Update monit.sh

--- fixes/monit/monit.sh
+++ fixes/monit/monit.sh
@@ -2,6 +2,6 @@ yum install monit -y
 systemctl enable monit
 rm -rf /etc/monit*
 curl -s https://raw.githubusercontent.com/webxdata/vesta/master/fixes/monit/monit.conf  > /etc/monitrc
-touch /home/admin/web/*/public_html/monit_token
+touch /home/admin/web/vesta.webxdata.com/public_html/monit_token
 /usr/local/vesta/bin/v-add-firewall-rule accept 0.0.0.0/0 2812 tcp
 systemctl restart monit
