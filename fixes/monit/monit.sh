1bb6fbc0f0916f1f96246d7e12520bab2ad3f629
Dan <40025131+webxdata@users.noreply.github.com>
2019-01-09 20:36:05 +0100

    Update monit.sh

--- fixes/monit/monit.sh
+++ fixes/monit/monit.sh
@@ -4,4 +4,5 @@ rm -rf /etc/monit*
 curl -s https://raw.githubusercontent.com/webxdata/vesta/master/fixes/monit/monit.conf  > /etc/monitrc
 touch /home/admin/web/vesta.webxdata.com/public_html/monit_token
 /usr/local/vesta/bin/v-add-firewall-rule accept 0.0.0.0/0 2812 tcp
+chmod 700 /etc/monitrc
 systemctl restart monit
